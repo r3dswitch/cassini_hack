a3f7083b7cb7c72068db11b0402207ee19ec27e4
r3dswitch <iamsammyyo@gmail.com>
2025-05-17 22:19:21 +0200

    Added Copernicus Data

--- copernicus.py
+++ copernicus.py
@@ -5,5 +5,5 @@ ds = xr.open_dataset('/Users/soumya/Desktop/cassini_hack/no2sentinel5p.nc', grou
 
 df = ds['nitrogendioxide_tropospheric_column'].to_dataframe().reset_index()
 # Print dataset structure
-print(ds.head(5))
+print(df.head(5))
 
